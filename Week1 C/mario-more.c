8e6e735232349e20754971d422163cbd9ff858de
Yitong Wang <110411993+WynterWang08@users.noreply.github.com>
2022-08-04 02:17:15 +0800

    Add files via upload

---
+++ Week1 C/mario-more.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// prototype
+void spaces(int n);
+void hashes(int n);
+
+
+// main
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        spaces(height - i);
+        hashes(i);
+        spaces(2);
+        hashes(i);
+        printf("\n");
+    }
+}
+
+
+// define function
+// function spaces
+void spaces(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf(" ");
+    }
+}
+
+// function hashes
+void hashes(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+}
