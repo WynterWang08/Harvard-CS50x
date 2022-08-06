6f7396694929594762578fe6df86895a0f9c0afa
Yitong Wang <110411993+WynterWang08@users.noreply.github.com>
2022-08-06 20:14:10 +0800

    Add files via upload

---
+++ Week5 Data Structures/speller_dictionary.c
@@ -0,0 +1,98 @@
+// Implements a dictionary's functionality
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+int dict_size = 0;
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    int num = hash(word);
+    node *n = table[num];
+    while (n != NULL)
+    {
+        if (strcasecmp(word, n->word) == 0)
+        {
+            return true;
+        }
+        n = n->next;
+    }
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    return toupper(word[0]) - 'A';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    FILE *dict = fopen(dictionary, "r");
+    if (dict == NULL)
+    {
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    while (fscanf(dict, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+        strcpy(n->word, word);
+        int num = hash(word);
+        n->next = table[num];
+        table[num] = n;
+        dict_size++;
+    }
+
+    fclose(dict);
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    return dict_size;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        node *n = table[i];
+        while (n != NULL)
+        {
+            node *tmp = n;
+            n = n->next;
+            free(tmp);
+        }
+        if (i == N - 1 && n == NULL)
+        {
+            return true;
+        }
+    }
+    return false;
+}
